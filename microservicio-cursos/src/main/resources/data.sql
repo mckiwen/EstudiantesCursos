c76a999031395ae9bbfea9713754e5f81883fbac
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-24 13:53:41 +0000

    + datos de ejemplo

---
+++ microservicio-cursos/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into cursos (nombre, descripcion, fecha_inicio, fecha_fin) VALUES ('SpringBoot', 'Curso de Spring Boot', '2024-01-15', '2024-06-15');
+insert into cursos (nombre, descripcion, fecha_inicio, fecha_fin) VALUES ('SpringSecurity', 'Curso de Spring Boot', '2024-01-15', '2024-06-15');
