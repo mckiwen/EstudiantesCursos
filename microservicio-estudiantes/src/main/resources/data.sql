3a9e6a8476bf4f82821d1e66ec23d7e94bb2017f
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-24 18:34:18 +0000

    + Update

---
+++ microservicio-estudiantes/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Antonio', 'Garcia', '1991-01-15');
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Maria', 'Ortiz', '1993-07-23');
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Pedro', 'Gomez', '1990-03-02');
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Carmen', 'Marrero', '1996-11-18');
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Carlos', 'Arias', '1994-10-01');
+insert into estudiantes (nombre, apellido, fecha_nacimiento) VALUES ('Monica', 'Estevez', '1992-05-20');
