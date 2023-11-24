4780ad8efeab2493a7829e22de172223bfaefe64
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-24 15:01:07 +0000

    + Update

--- microservicio-cursos/src/main/resources/data.sql
+++ microservicio-cursos/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into cursos (nombre, descripcion, fecha_inicio, fecha_fin) VALUES ('SpringBoot', 'Curso de Spring Boot', '2024-01-15', '2024-06-15');
 insert into cursos (nombre, descripcion, fecha_inicio, fecha_fin) VALUES ('SpringSecurity', 'Curso de Spring Boot', '2024-01-15', '2024-06-15');
+insert into cursos (nombre, descripcion, fecha_inicio, fecha_fin) VALUES ('SpringData', 'Curso de Spring Data', '2024-08-15', '2024-12-15');
