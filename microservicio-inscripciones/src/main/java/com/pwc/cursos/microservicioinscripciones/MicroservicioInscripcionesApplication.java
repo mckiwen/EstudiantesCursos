970bffa2c97448697feb3e66bdc99f645f438387
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-25 11:26:28 +0000

    + Microservicio-inscripciones creado

---
+++ microservicio-inscripciones/src/main/java/com/pwc/cursos/microservicioinscripciones/MicroservicioInscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.pwc.cursos.microservicioinscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioInscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioInscripcionesApplication.class, args);
+	}
+
+}
---
+++ microservicio-inscripciones/src/test/java/com/pwc/cursos/microservicioinscripciones/MicroservicioInscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwc.cursos.microservicioinscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioInscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
