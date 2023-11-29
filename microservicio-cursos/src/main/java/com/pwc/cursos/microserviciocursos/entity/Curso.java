1db9431fd0b370d49f75442ba37ee77af8616a14
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-29 14:17:16 +0000

    + update

--- microservicio-cursos/src/main/java/com/pwc/cursos/microserviciocursos/entity/Curso.java
+++ microservicio-cursos/src/main/java/com/pwc/cursos/microserviciocursos/entity/Curso.java
@@ -1,10 +1,15 @@
 package com.pwc.cursos.microserviciocursos.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
@@ -20,14 +25,18 @@ public class Curso {
     private Long id;
 
     @Schema(description = "Nombre del Curso", example = "SpringBoot")
+    @Nonnull
     private String nombre;
 
     @Schema(description = "Descripcion del curso", example = "Curso de Spring Boot")
+    @Nonnull
     private String descripcion;
 
     @Schema(description = "Fecha de inicio", example = "2024-01-15")
+    @Nonnull
     private LocalDate fechaInicio;
 
     @Schema(description = "Fecha de finalizacion", example = "2024-06-15")
+    @Nonnull
     private LocalDate fechaFin;
 }
